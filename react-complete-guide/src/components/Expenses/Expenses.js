df7650d1817ffe4ca82fef8414ce2abd567d985d
Kevin Wong <Gadzai@hotmail.com>
2023-04-02 11:13:17 +0800

    adding chart components

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilterYear] = useState("2020");
@@ -27,6 +28,7 @@ const Expenses = (props) => {
             selected={filteredYear}
             onFilter={expenseFilterHandler}
           ></ExpensesFilter>
+          <ExpensesChart expenses={filteredExpenses}/>
           <ExpensesList items={filteredExpenses} />
         </Card>
       </li>
